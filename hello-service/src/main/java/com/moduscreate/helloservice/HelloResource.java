d354516256fecc22eed44e1aa90bdf1975f3a30f
Wesley Fuchter <wesley.fuchter@gmail.com>
2020-05-07 15:59:41 -0400

    create the service with the dummy hello endpoint.

---
+++ hello-service/src/main/java/com/moduscreate/helloservice/HelloResource.java
@@ -0,0 +1,17 @@
+package com.moduscreate.helloservice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from " + System.getenv().getOrDefault("HOSTNAME", "unknown");
+    }
+
+}
---
+++ hello-service/src/test/java/com/moduscreate/helloservice/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.moduscreate.helloservice;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
