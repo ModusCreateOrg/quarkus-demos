05051ecdb3c5ea434bf14c90761505b5882c0fff
Wesley Fuchter <wesley.fuchter@gmail.com>
2020-05-11 14:48:16 -0400

    create endpoint to force errors on the server.

--- hello-service/src/main/java/com/moduscreate/helloservice/HelloResource.java
+++ hello-service/src/main/java/com/moduscreate/helloservice/HelloResource.java
@@ -4,14 +4,28 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class HelloResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from " + System.getenv().getOrDefault("HOSTNAME", "unknown");
+    public Response hello() {
+        if (HelloHealthCheck.respondWithError) {
+            return Response.serverError().build();
+        } else {
+            return Response.ok("Hello from " +
+                    System.getenv().getOrDefault("HOSTNAME", "unknown")).build();
+        }
+    }
+
+    @GET
+    @Path("/toggleErrorResponse")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response changeRespondWithErrorValue() {
+        HelloHealthCheck.respondWithError = !HelloHealthCheck.respondWithError;
+        return Response.ok().entity(HelloHealthCheck.respondWithError).build();
     }
 
 }
