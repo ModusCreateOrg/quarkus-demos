4f2a5c8d96eb92d71391ef92a9455f6278adfd8c
Wesley Fuchter <wesley.fuchter@moduscreate.com>
2020-10-06 17:20:57 -0300

    Example for the YT demo about Quarkus with Panache and Kotlin (#4)
    * initial commit
    
    * hibernate validator
    
    * additional filters

---
+++ user-service/src/main/kotlin/com/moduscreate/User.kt
@@ -0,0 +1,56 @@
+package com.moduscreate
+
+import io.quarkus.hibernate.orm.panache.kotlin.PanacheCompanion
+import io.quarkus.hibernate.orm.panache.kotlin.PanacheEntityBase
+import org.hibernate.annotations.GenericGenerator
+import java.time.LocalDate
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.Table
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.PastOrPresent
+
+@Entity
+@Table(name = "\"User\"")
+class User: PanacheEntityBase {
+
+    companion object: PanacheCompanion<User, String> {
+        fun findByEmail(email: String) = find("email", email).firstResult()
+        fun findByStatus(status: Status) = find("status", status).list()
+    }
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false)
+    lateinit var id: String
+
+    @Column
+    @NotNull
+    lateinit var email: String
+
+    @Column
+    @NotNull
+    lateinit var password: String
+
+    @Column
+    lateinit var name: String
+
+    @Column(name = "birth_date")
+    @PastOrPresent
+    @NotNull
+    lateinit var birthDate: LocalDate
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    var status: Status = Status.ACTIVE
+
+}
+
+enum class Status {
+    ACTIVE, INACTIVE, BLOCKED
+}
---
+++ user-service/src/main/kotlin/com/moduscreate/UserResource.kt
@@ -0,0 +1,47 @@
+package com.moduscreate
+
+import java.net.URI
+import javax.transaction.Transactional
+import javax.validation.Valid
+import javax.ws.rs.Consumes
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
+@Path("/user")
+class UserResource {
+
+    @POST
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    fun save(@Valid user: User): Response =
+            user.let {
+                it.persist()
+                Response.created(URI("/user/${user.id}")).entity(user).build()
+            }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun findById(@PathParam("id") id: String): Response =
+            Response.ok(User.findById(id)).build()
+
+    @GET
+    @Path("/by-email/")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun findByEmail(@QueryParam("email") email: String): Response =
+            Response.ok(User.findByEmail(email)).build()
+
+    @GET
+    @Path("/by-status/")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun findByStatus(@QueryParam("status") status: String): Response =
+            Response.ok(User.findByStatus(Status.valueOf(status))).build()
+
+}
